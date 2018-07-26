170317b08f447490fd9adfa9f0727afef88a524f
Kristiyan Goleminov <kristiyan.goleminov@primeholding.com>
2018-07-26 15:34:45 +0300

    WIP Admin panel implemented

---
+++ BeerPal/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace BeerPal.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return RedirectToActionPermanent("Index", "Sales");
+        }        
+    }
+}
